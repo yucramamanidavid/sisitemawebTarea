f655aa1cdd65dd0c0fa31c0940f81377fd0d034f
alexs773 <alexdk828732@gmail.com>
2025-10-02 18:50:44 -0500

    casi

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.secret_key = 'tu_clave_secreta_aqui'
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host='localhost',
+            host='3.95.201.54',
             user='root',
             password='',
             database='gestion_tareas',
